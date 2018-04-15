5cac252c453dabaf7a9464edea9f60b8cdf53e25
Benjamin Brahmer <info@b-brahmer.de>
2018-04-15 17:45:26 +0200

    reformat target list - test

--- build.py
+++ build.py
@@ -15,11 +15,21 @@ import subprocess as sp
 #
 
 DEFAULTS = {
-    'targets' : ['ar71xx-generic', 'ar71xx-tiny', 'ar71xx-nand',
-                 'brcm2708-bcm2708', 'brcm2708-bcm2709', 'mpc85xx-generic',
-                 'ramips-mt7621', 'sunxi-cortexa7', 'x86-generic',
-                 'x86-geode', 'x86-64', 'ramips-mt7620',
-                 'ramips-mt76x8', 'ramips-rt305x'],
+    'targets' : ['ar71xx-generic',
+                 'ar71xx-tiny',
+                 'ar71xx-nand',
+                 #'brcm2708-bcm2708',
+                 #'brcm2708-bcm2709',
+                 'mpc85xx-generic',
+                 'ramips-mt7621',
+                 #'sunxi-cortexa7',
+                 'x86-generic',
+                 'x86-geode',
+                 'x86-64',
+                 'ramips-mt7620',
+                 'ramips-mt76x8',
+                 #'ramips-rt305x'
+                ],
     'gluon_dir': '/gluon',
     'make_cores' : "1",
     'make_loglevel': 'V=s',
