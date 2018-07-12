8b429fd9220453281f126bd1f7ad2c18089a444a
Benjamin Brahmer <info@b-brahmer.de>
2018-07-12 09:54:05 +0200

    update target list

--- build.py
+++ build.py
@@ -12,23 +12,23 @@ import os
 import time
 from datetime import datetime
 import subprocess as sp
-#
 
 DEFAULTS = {
     'targets' : ['ar71xx-generic',
                  'ar71xx-tiny',
                  'ar71xx-nand',
-                 #'brcm2708-bcm2708',
-                 #'brcm2708-bcm2709',
+                 'brcm2708-bcm2708',
+                 'brcm2708-bcm2709',
                  'mpc85xx-generic',
                  'ramips-mt7621',
-                 #'sunxi-cortexa7',
+                 'sunxi',
                  'x86-generic',
                  'x86-geode',
                  'x86-64',
+                 'ipq806x',
                  'ramips-mt7620',
-                 'ramips-mt76x8',
-                 #'ramips-rt305x'
+                 'ramips-mt7628',
+                 'ramips-rt305x'
                 ],
     'gluon_dir': '/gluon',
     'make_cores' : "1",
