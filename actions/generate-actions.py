b90f635142dd9d6f5a7d6edf7d915dc56eaecae9
Benjamin Brahmer <info@b-brahmer.de>
2020-05-29 20:19:10 +0200

    finalize actions

--- actions/generate-actions.py
+++ actions/generate-actions.py
@@ -4,15 +4,15 @@ import sys
 
 ACTIONS_HEAD = """
 # Update this file after adding/removing/renaming a target by running
-# `make list-targets BROKEN=1 | ./contrib/actions/generate-actions.py > ./.github/workflows/build-gluon.yml`
+# `make list-targets BROKEN=1 | ../actions/generate-actions.py > ../.github/workflows/build-gluon.yml`
 
 name: Build Gluon
 on:
   push:
     branches:
-      - master
-      - next
-      - v20*
+      - testing
+      - rc
+      - stable
   pull_request:
     types: [opened, synchronize, reopened]
 jobs:
@@ -24,21 +24,23 @@ ACTIONS_TARGET="""
     runs-on: ubuntu-latest
     steps:
       - uses: actions/checkout@v1
+      - name: Checkout submodules
+        uses: textbook/git-checkout-submodule-action@master
       - name: Install Dependencies
-        run: sudo contrib/actions/install-dependencies.sh
+        run: sudo actions/install-dependencies.sh
       - name: Build
-        run: contrib/actions/run-build.sh {target_name}
+        run: actions/run-build.sh {target_name}
       - name: Archive build logs
         if: ${{{{ !cancelled() }}}}
         uses: actions/upload-artifact@v1
         with:
           name: {target_name}_logs
-          path: openwrt/logs
+          path: gluon/openwrt/logs
       - name: Archive build output
         uses: actions/upload-artifact@v1
         with:
           name: {target_name}_output
-          path: output
+          path: gluon/output
 """
 
 output = ACTIONS_HEAD
--- actions/run-build.sh
+++ actions/run-build.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-export BROKEN=1
+export GLUON_RELEASE=gluon-ffsh-2020.1.2.0
+
+
+export BROKEN=0
 export GLUON_AUTOREMOVE=1
-export GLUON_DEPRECATED=1
+export GLUON_DEPRECATED ?= upgrade
 export GLUON_SITEDIR="../"
 export GLUON_TARGET=$1
 export BUILD_LOG=1
