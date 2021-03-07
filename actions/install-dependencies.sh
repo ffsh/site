0977a5a7c7da7f43202d716e693332c91a4e60bb
Benjamin Brahmer <info@b-brahmer.de>
2021-03-07 12:52:00 +0100

    actions switcht to python2

--- actions/install-dependencies.sh
+++ actions/install-dependencies.sh
@@ -5,6 +5,6 @@ set -e
 cp actions/sources.list /etc/apt/sources.list
 rm -rf /etc/apt/sources.list.d
 apt update
-apt install git subversion build-essential python gawk unzip libncurses5-dev zlib1g-dev libssl-dev wget time
+apt install git subversion build-essential python2 gawk unzip libncurses5-dev zlib1g-dev libssl-dev wget time
 apt clean
 rm -rf /var/lib/apt/lists/*
