69a86a72f8ceb1efc16a0462f796ef43dfb6f4f6
Benjamin Brahmer <info@b-brahmer.de>
2021-03-07 12:07:59 +0100

    Adjust site.mk

--- site.mk
+++ site.mk
@@ -4,8 +4,7 @@ GLUON_FEATURES := \
     ebtables-filter-ra-dhcp \
     ebtables-limit-arp \
     mesh-batman-adv-15 \
-    mesh-vpn-fastd \
-    web-mesh-vpn-fastd \
+    mesh-vpn-wireguard \
     radv-filterd \
     respondd \
     status-page \
