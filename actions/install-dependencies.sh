79c31f49230f177001440752894c47aca373baeb
Benjamin Brahmer <info@b-brahmer.de>
2026-01-01 04:19:16 +0100

    update build deps

--- actions/install-dependencies.sh
+++ actions/install-dependencies.sh
@@ -3,6 +3,6 @@
 set -e
 
 apt-get -y update
-apt-get -y install git subversion build-essential python3 python3-elftools gawk unzip libncurses5-dev zlib1g-dev libssl-dev wget time qemu-utils libelf-dev clang llvm pkg-config rsync gettext file patch
+apt-get -y install git subversion build-essential python3 python3-elftools gawk unzip libncurses-dev zlib1g-dev libssl-dev wget time qemu-utils libelf-dev clang llvm pkg-config rsync gettext file patch device-tree-compiler python3-yaml
 apt-get -y clean
 rm -rf /var/lib/apt/lists/*
