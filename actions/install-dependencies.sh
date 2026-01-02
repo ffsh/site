1ac658e0d79e629f6e3695df48ae5f069131e64e
Benjamin Brahmer <info@b-brahmer.de>
2026-01-02 15:08:58 +0700

    add more deps

--- actions/install-dependencies.sh
+++ actions/install-dependencies.sh
@@ -3,6 +3,6 @@
 set -e
 
 apt-get -y update
-apt-get -y install git subversion build-essential python3 python3-pyelftools gawk unzip libncurses-dev zlib1g-dev libssl-dev wget time qemu-utils libelf-dev clang llvm pkg-config rsync gettext file patch device-tree-compiler python3-yaml
+apt-get -y install git subversion build-essential python3 python3-pyelftools gawk unzip libncurses-dev zlib1g-dev libssl-dev wget time qemu-utils libelf-dev clang llvm pkg-config rsync gettext file patch device-tree-compiler python3-yaml python3-dev python3-is-python3 bison flex u-boot-tools swig
 apt-get -y clean
 rm -rf /var/lib/apt/lists/*
